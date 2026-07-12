a60ae03dd33bd9b0220ecd6924d9109dee3a1735
sri <srilaxmi1247@gmail.com>
2026-07-12 10:29:44 +0530

    update volunteer profile and list

--- js/volunteer-profile.js
+++ js/volunteer-profile.js
@@ -3,40 +3,46 @@ document.addEventListener("DOMContentLoaded", () => {
     const volunteer = JSON.parse(localStorage.getItem("selectedVolunteer"));
 
     if (!volunteer) {
-        alert("Volunteer information not found.");
+        alert("Volunteer not found.");
         window.location.href = "volunteer-list.html";
         return;
     }
 
-    // Avatar
     const avatar = document.getElementById("avatar");
 
-    if (volunteer.name) {
-        const initials = volunteer.name
-            .split(" ")
-            .map(word => word.charAt(0))
-            .join("")
-            .toUpperCase();
+    const initials = volunteer.name
+        .split(" ")
+        .map(word => word[0])
+        .join("")
+        .toUpperCase();
 
-        avatar.textContent = initials;
-    }
+    avatar.textContent = initials;
 
-    // Header
     document.getElementById("volunteerName").textContent = volunteer.name;
     document.getElementById("specialization").textContent = volunteer.specialization;
 
-    // Information
     document.getElementById("gender").textContent = volunteer.gender;
     document.getElementById("age").textContent = volunteer.age + " Years";
     document.getElementById("experience").textContent = volunteer.experience;
     document.getElementById("languages").textContent =
         volunteer.languages.join(", ");
-
     document.getElementById("phone").textContent = volunteer.phone;
-
     document.getElementById("availability").textContent =
         volunteer.availability;
-
     document.getElementById("rating").textContent =
         "⭐ " + volunteer.rating + " / 5";
+
 });
+
+function requestVolunteer(){
+
+    const volunteer = JSON.parse(localStorage.getItem("selectedVolunteer"));
+
+    alert(
+        "Volunteer request sent successfully!\n\n" +
+        "Volunteer: " + volunteer.name
+    );
+
+    window.location.href = "dashboard.html";
+
+}
